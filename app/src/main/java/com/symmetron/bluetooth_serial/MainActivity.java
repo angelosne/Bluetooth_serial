42c67f10fa895db254b7ca9932b184bb18a623b1
Angelosne <negkas.aggelos@gmail.com>
2020-02-05 19:52:57 +0200

    commit all

---
+++ app/src/main/java/com/symmetron/bluetooth_serial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.symmetron.bluetooth_serial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
